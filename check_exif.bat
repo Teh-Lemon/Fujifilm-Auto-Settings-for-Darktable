bbd9760c4bda699c0e75bd33114b7b5520ebed05
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2022-07-16 07:44:04 +0100

    Added an exif checker batch file

---
+++ check_exif.bat
@@ -0,0 +1,11 @@
+REM Shows the exif that this script uses of all your photos
+REM To use: Place this .bat file in the folder your want to test and double-click it
+
+FOR %%i IN (*.jpg) DO (
+	exiftool -AutoDynamicRange %%i
+	exiftool -DevelopmentDynamicRange %%i
+	exiftool -RawImageAspectRatio %%i
+	exiftool -FilmMode %%i
+	exiftool -Saturation %%i
+	)
+pause
