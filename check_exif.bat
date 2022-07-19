036906a4c99c1993a854b5e8c3d47f0df138a614
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2022-07-19 01:15:29 +0100

    Fixed exif checker bat file to use the raws not the jpg

--- check_exif.bat
+++ check_exif.bat
@@ -1,7 +1,7 @@
 REM Shows the exif that this script uses of all your photos
 REM To use: Place this .bat file in the folder your want to test and double-click it
 
-FOR %%i IN (*.jpg) DO (
+FOR %%i IN (*.raf) DO (
 	exiftool -AutoDynamicRange %%i
 	exiftool -DevelopmentDynamicRange %%i
 	exiftool -RawImageAspectRatio %%i
