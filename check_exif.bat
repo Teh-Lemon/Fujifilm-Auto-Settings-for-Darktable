45613ae3e7cca352d190e02e87d6d4b6c12b70d5
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2022-07-20 09:09:00 +0100

    Massively sped up check_exif.bat Now only using 1 exiftool call per file instead of 1 for each tag per file.

--- check_exif.bat
+++ check_exif.bat
@@ -1,12 +1,4 @@
-REM Shows the exif that this script uses of all your photos
-REM To use: Place this .bat file in the folder your want to test and double-click it
+:: Shows the exif that this script uses of all your photos
+:: To use: Place this .bat file in the folder your want to test and double-click it
 
-FOR %%i IN (*.raf) DO (
-	exiftool -AutoDynamicRange %%i
-	exiftool -DevelopmentDynamicRange %%i
-	exiftool -RawImageAspectRatio %%i
-	exiftool -Orientation %%i
-	exiftool -FilmMode %%i
-	exiftool -Saturation %%i
-	)
-pause
+exiftool -AutoDynamicRange -DevelopmentDynamicRange -RawImageAspectRatio -Orientation -FilmMode -Saturation -k *.raf
